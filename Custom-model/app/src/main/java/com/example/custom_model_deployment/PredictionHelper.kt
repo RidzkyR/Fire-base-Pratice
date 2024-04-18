f8ec5433b5ca37406c7ade8cedf94a41f249fd05
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-19 01:48:39 +0700

    add function initializeInterpreter

--- Custom-model/app/src/main/java/com/example/custom_model_deployment/PredictionHelper.kt
+++ Custom-model/app/src/main/java/com/example/custom_model_deployment/PredictionHelper.kt
@@ -2,9 +2,12 @@ package com.example.custom_model_deployment
 
 import android.content.Context
 import android.content.res.AssetManager
+import android.util.Log
 import com.google.android.gms.tflite.client.TfLiteInitializationOptions
 import com.google.android.gms.tflite.gpu.support.TfLiteGpu
 import com.google.android.gms.tflite.java.TfLite
+import org.tensorflow.lite.InterpreterApi
+import org.tensorflow.lite.gpu.GpuDelegateFactory
 import java.io.FileInputStream
 import java.io.IOException
 import java.nio.ByteBuffer
@@ -19,6 +22,7 @@ class PredictionHelper(
     private val onError: (String) -> Unit,
 ) {
     private var isGPUSupported: Boolean = false
+    private var interpreter: InterpreterApi? = null
 
     // inisialisasi TFLite dari Google Play Service
     init {
@@ -42,9 +46,31 @@ class PredictionHelper(
     private fun loadLocalModel() {
         try {
             val buffer: ByteBuffer = loadModelFile(context.assets, modelName)
+            initializeInterpreter(buffer)
         }catch (ioExeption: IOException){
 
         }
+    }
+
+    private fun initializeInterpreter(model: Any) {
+        interpreter?.close()
+        try {
+            val options = InterpreterApi.Options()
+                .setRuntime(InterpreterApi.Options.TfLiteRuntime.FROM_SYSTEM_ONLY)
+            if (isGPUSupported){
+                options.addDelegateFactory(GpuDelegateFactory())
+            }
+            if (model is ByteBuffer) {
+                interpreter = InterpreterApi.create(model, options)
+            }
+        } catch (e: Exception) {
+            onError(e.message.toString())
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
+    fun close(){
+        interpreter?.close()
     }
 
     private fun loadModelFile(assetManager: AssetManager, modelPath: String): MappedByteBuffer{
