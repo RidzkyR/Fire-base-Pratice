680acfaefa5b2134e1b797d305df98ecd3ac3018
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-19 01:57:06 +0700

    stater project update

--- Custom-model/app/build.gradle.kts
+++ Custom-model/app/build.gradle.kts
@@ -44,11 +44,8 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.material.v1100)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.tensorflow.lite.support)
-    implementation(libs.tensorflow.lite.metadata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
